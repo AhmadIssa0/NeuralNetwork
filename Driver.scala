7779c58341ac8a6f3666d9253399e882139d9019
Ahmad Issa <ahmadissa@gmail.com>
2017-01-15 11:24:45 -0600

    Added backprop, cross-entropy cost, L2 regularization

--- Driver.scala
+++ Driver.scala
@@ -1,11 +1,41 @@
 
-import breeze.linalg._
+package neuralnetwork
+import breeze.linalg.{Vector => _, _}
 
 object Driver {
 
   def main(args: Array[String]): Unit = {
-    val x = DenseVector.zeros[Double](5)
-    println(x)
+    var n = Network(Vector(2, 2, 1))
+    println(n)
+    println(n.output(DenseVector(0.,0.)))
+
+    val training = Vector(
+      (DenseVector(0.,0.), DenseVector(0.)),
+      (DenseVector(0.,1.), DenseVector(1.)),
+      (DenseVector(1.,0.), DenseVector(1.)),
+      (DenseVector(1.,1.), DenseVector(0.))
+    )
+
+    val r = scala.util.Random
+    for (i <- 0 until 500000) {
+      n = n.descend(Vector(training(r.nextInt(4))),
+        learningRate=0.05,
+        lambda=0.002
+      )
+    }
+
+    println("0, 0 -> " + n.output(DenseVector(0.,0.)))
+    println("0, 1 -> " + n.output(DenseVector(0.,1.)))
+    println("1, 0 -> " + n.output(DenseVector(1.,0.)))
+    println("1, 1 -> " + n.output(DenseVector(1.,1.)))
+
+    println(n)
+
+    //println(n.backprop(DenseVector(0., 0.), DenseVector(0.)))
+    //println(n)
+    //println(n.output(DenseVector(Array(0., 0.))))
+    //println(DenseMatrix.tabulate(3, 2)(_ + _))
+    //println(10 - DenseMatrix.tabulate(3,2)(_ + _))
   }
 
 }
--- Network.scala
+++ Network.scala
@@ -1,6 +1,8 @@
 
+package neuralnetwork
 
-import breeze.linalg._
+import breeze.linalg.{Vector => _, _}
+import breeze.numerics._
 
 case class Network private(
   size: Vector[Int],
@@ -9,27 +11,105 @@ case class Network private(
   biases: Vector[DenseVector[Double]]) {
 
 
-  def output(input: DenseVector[Double]): DenseVector[Double] = ???
+  def output(input: DenseVector[Double]): DenseVector[Double] = {
+    require(input.length == size(0), "Input vector to NN of wrong size.")
+
+    (0 until weights.length).foldLeft(input) { (a, i) =>
+      sigmoid(weights(i) * a + biases(i))
+    }
+  }
+
+  def backprop(input: DenseVector[Double],
+    output: DenseVector[Double]
+  ): (Vector[DenseMatrix[Double]], Vector[DenseVector[Double]]) = {
+    require(input.length == size(0) && output.length == size(size.length - 1))
+
+    // Computed predicted output and Z's
+    val (predOutput, zs) = 
+      (0 until weights.length).foldLeft((input, Vector[DenseVector[Double]]())) {
+        case ((a, zs), i) =>
+          val z = weights(i) * a + biases(i)
+        (sigmoid(z), zs :+ z)
+      }
+
+    //println("zs: " + zs)
+    def sigmoidDeriv(v: DenseVector[Double]) = sigmoid(v) :* (1. - sigmoid(v))
+
+    // Last layer
+    // cross-entropy
+    val crossEntDeriv = (-(output :/ predOutput) + ((1. - output) :/ (1. - predOutput)))
+    val deltaL = crossEntDeriv  :* sigmoidDeriv(zs.last)
+    // MSE
+    //val deltaL = (predOutput - output) :* sigmoidDeriv(zs.last)
+    //println("deltaL: " + deltaL)
+    //println("weights: ")
+    //weights.foreach { w => println("s" + w) }
+    val deltas = 
+    (weights.length - 1 to 1 by -1).foldLeft(Vector[DenseVector[Double]](deltaL)) {
+      (deltas, i) =>
+        ((weights(i).t * deltas(0)) :* sigmoidDeriv(zs(i - 1))) +: deltas
+    }
+
+    val as = input +: zs.map(sigmoid(_))
+
+    val gradWs = deltas.zip(as).map { case (d, a) => d * a.t }
+
+    (gradWs, deltas)
+  }
+
+  def descend(miniBatch: Vector[(DenseVector[Double], DenseVector[Double])],
+    learningRate: Double = 1.0,
+    lambda: Double = 0.001 // regularization parameter
+  ): Network = {
+
+    val alpha = learningRate / miniBatch.length
+    val weightDecay = (1 - learningRate * lambda / miniBatch.length)
+
+    val decayedW = weights.map(_ :* weightDecay)
+    
+    val (nWeight, nBias) = 
+    miniBatch.foldLeft((decayedW, biases)) {
+      case ((w, b), t) =>
+        val (w1, b1) = backprop(t._1, t._2)
+        ((0 until w.length).toVector.map(i => w(i) - (alpha :* w1(i))),
+         (0 until b.length).toVector.map(i => b(i) - (alpha :* b1(i))))
+    }
+
+    this.copy(weights = nWeight, biases = nBias)
+  }
+
 }
 
 
 object Network {
+
+  /**
+    * @param size Number of nodes in each layer, starting with input layer
+    *             ending with output layer.
+    */
   def apply(size: Vector[Int]): Network = {
-    require(size.forall(s => s > 0))
+    require(size.length > 0 && size.forall(s => s > 0))
+
+    /* Recommended in: http://stats.stackexchange.com/a/186351 */
+    def randomInitialize(size_in: Int, size_out: Int): Double = {
+      val r = scala.util.Random    
+      val epsilon = 2*math.sqrt(6 / (size_in + size_out))
+      r.nextDouble * 2 * epsilon - epsilon   
+    }
+
+    val weights = size
+      .sliding(2)
+      .toVector 
+      .map { case Vector(n, m) =>
+        DenseMatrix.tabulate(m, n)((_, _) => randomInitialize(n, m))
+      }
 
     Network(size = size,
       nLayers = size.length,
-      weights = Vector(),
-      bias = Vecotr()) 
+      weights = weights,
+      biases = size.tail.map(n => DenseVector.zeros[Double](n))
+    )
   }
-
-  def randomInitialize(size_in: Int, size_out: Int): double = {
-   val r = scala.util.Random    
-   val epsilon = 4*sqrt(6/(size_in+size_out))
-   r.nextDouble*2*epsilon-epslon   
- }
-
 }
 
 
-val n = Network(Vector(3, 2, 2, 1))
